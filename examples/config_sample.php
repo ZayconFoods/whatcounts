989d750e9b26eb9c7569f8eba358de5611776918
Tony DeStefano <spokanetony@gmail.com>
2016-01-21 14:36:02 -0800

    Created the config_sample.php file for testing in the examples folder

---
+++ examples/config_sample.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tony DeStefano
+ * Date: 1/21/16
+ * Time: 2:31 PM
+ *
+ * Copy to config.php and input your realm and password for testing
+ */
+
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require_once( dirname( __DIR__ ) . '/src/whatcounts_required.php' );
+
+define( 'WC_REALM', '[YOUR_REALM]');
+define( 'WC_PASSWORD', '[YOUR_PASSWORD]' );
